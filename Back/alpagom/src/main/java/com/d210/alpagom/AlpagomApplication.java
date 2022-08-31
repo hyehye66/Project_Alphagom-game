c325544c90bb1d7b4f33ea4fc500b424a34a01b5
박경열 <hrq789@naver.com>
2022-08-31 14:58:14 +0900

    feat : 프로젝트 초기 설정 (#1)

---
+++ Back/alpagom/src/main/java/com/d210/alpagom/AlpagomApplication.java
@@ -0,0 +1,13 @@
+package com.d210.alpagom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpagomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlpagomApplication.class, args);
+	}
+
+}
---
+++ Back/alpagom/src/test/java/com/d210/alpagom/AlpagomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d210.alpagom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpagomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
