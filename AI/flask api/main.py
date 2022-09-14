3d9577d2fc6ebdc5f0ddc5fece8d79464faf6fc6
배근혜 <odille2992@naver.com>
2022-09-14 18:17:18 +0900

    [S07P22D210-211] STT 구현

--- AI/flask api/main.py
+++ AI/flask api/main.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
+import speech_recognition as sr
 
 app = Flask(__name__)
+r=sr.Recognizer()
 
-from datetime import datetime
-@app.route('/test')
-def whattime():
-  now = datetime.now()
-  return render_template('now.html', now=now)
+@app.route("/api/ai/check", methods=["GET"])
+def index():
+    transcript = ''
+    # while transcript not in response:
+    while not transcript:
+        with sr.Microphone() as source:
+            audio=r.listen(source)
+            transcript=r.recognize_google(audio, language="ko-KR")
+        
+    # return render_template('index.html', transcript=transcript)
+    return transcript     
+
+# from datetime import datetime
+# @app.route('/test')
+# def whattime():
+#   now = datetime.now()
+#   return render_template('now.html', now=now)      
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(debug=True, threaded=True)
+
+# if __name__ == "__main__":
+#     app.run(host='127.0.0.1', port=5000, debug=False)
