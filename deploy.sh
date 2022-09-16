9c0b3f44f205595f52b26f44be7ebb3c40de1932
mihyun0921 <mihyun0921@gmail.com>
2022-09-16 05:05:00 +0000

    [S07P22D210-120] deploy.sh 파일 수정

--- deploy.sh
+++ deploy.sh
@@ -31,4 +31,10 @@ if [ -n "$EXIST_AFTER" ]; then
     # 이전 컨테이너 종료
     docker-compose -p ${DOCKER_APP_NAME}-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
     echo "$BEFORE_COMPOSE_COLOR down"
+
+    # 이전 이미지 삭제
+    # 동일한 태그를 가진 Docker 이미지가 빌드될 경우, 기존에 있던 이미지는 삭제되지는 않고, tag가 none으로 변경된 상태로 남아 있게 된다.
+    # 더 이상 컨테이너에 연결되지 않고, 태그가 없어진 이미지를 Dangling image라고 한다.
+    # 이러한 dangling image를 그대로 방치하면 파일시스템 용량을 차지하게 되고, Docker 이미지 확인에도 불편함이 있을 수 있기 때문에 삭제.
+    docker rmi $(docker images -f "dangling=true" -q)
 fi
