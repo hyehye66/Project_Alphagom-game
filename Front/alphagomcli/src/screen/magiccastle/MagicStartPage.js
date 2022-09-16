6e99b07eb0dcc92edc7677325317a55201f9c25c
Hyunjung Lee <pycbcat@gmail.com>
2022-09-16 17:02:58 +0900

    [S07P22D210-169] setting - RN CLI 재구성

---
+++ Front/alphagomcli/src/screen/magiccastle/MagicStartPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+
+import Footer from '../../Footer';
+
+function MagicStartPage () {
+
+    return (
+    <View>
+        <Text>
+            마법의 성 구간 시작 입니다.
+        </Text>
+        <Footer />
+    </View>
+    );
+};
+
+export default MagicStartPage;
