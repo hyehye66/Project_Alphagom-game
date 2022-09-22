a0f74559f4fff607138c46181ac46cf7b79e40f9
이미현 <mihyun0921@gmail.com>
2022-09-22 14:22:29 +0900

    [S07P22D210-232] feature - social login google

--- Back/alphagom/src/main/java/com/d210/alphagom/domain/entity/User.java
+++ Back/alphagom/src/main/java/com/d210/alphagom/domain/entity/User.java
@@ -1,6 +1,7 @@
 package com.d210.alphagom.domain.entity;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,22 +13,47 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user")
 public class User extends BaseTimeEntity {
+    // BaseTimeEntity : 모든 Entity의 상위 클래스에서 createdDate, updateDate를 자동으로 관리해주는 역할
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private String email;
 
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    /*
+     * 추가 정보
+     */
     private String nickname;
 
     private LocalDate birth;
 
-    public User(String name, String email, String nickname, LocalDate birth) {
+    @Builder
+    public User(String name, String email, String picture, Role role){
         this.name = name;
         this.email = email;
-        this.nickname = nickname;
-        this.birth = birth;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture){
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
     }
 }
