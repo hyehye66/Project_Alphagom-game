6a36196bbe602e049a1a00bb67c223abd0d6c1ec
김수빈 <foursoob@naver.com>
2022-09-24 12:59:20 +0900

    Feature/front/setting page

---
+++ Front/alphagomcli/src/screen/otherpage/DrawerNavigator.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StackNavigator } from "./StackNavigator";
+
+import HomePage from "./HomePage";
+import Settings from "./Settings"
+import CustomDrawer from "./CustomDrawer";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <Drawer.Navigator
+        drawerContent={props => <CustomDrawer {...props} />}
+        initialRouteName="HomePageDrawer"
+        screenOptions={{
+          headerShown: false,
+          drawerActiveBackgroundColor: '#aa18ea',
+          drawerActiveTintColor: '#fff',
+        }}
+      >
+        <Drawer.Screen
+          name='메뉴' 
+          component={StackNavigator} 
+          options={{ drawerLabel: '메뉴' }} />
+        <Drawer.Screen 
+          name='처음으로' 
+          component={HomePage} 
+          options={{ drawerLabel: '처음으로' }} />
+        <Drawer.Screen 
+          name='설정하기' 
+          component={Settings} 
+          options={{ drawerLabel: '설정하기' }} />
+      </Drawer.Navigator>
+    </View>
+  );
+}
+
+
+export default DrawerNavigator;
