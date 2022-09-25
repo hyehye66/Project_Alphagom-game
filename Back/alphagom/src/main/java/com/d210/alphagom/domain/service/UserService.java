a7befeb37f8c265784670cc5948b6ac9df92d18e
박경열 <hrq789@naver.com>
2022-09-25 22:20:35 +0900

    [S07P22D210-241] feature - 마법의 성 API 구현

--- Back/alphagom/src/main/java/com/d210/alphagom/domain/service/UserService.java
+++ Back/alphagom/src/main/java/com/d210/alphagom/domain/service/UserService.java
@@ -20,7 +20,21 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 userId : " + userId + " 는 존재하지 않습니다."));
     }
 
+    @Transactional
+    public String saveNickname(Long userId, String nickname) {
+        User user = userRepository.findById(userId).get();
+        user.setNickname(nickname);
+        return nickname;
+    }
+
     public String findUserNickName(Long userId) {
         return userRepository.findById(userId).get().getNickname();
+    }
+
+    @Transactional
+    public Long checkIsCastle(Long userId) {
+        User user = userRepository.findById(userId).get();
+        user.updateIsCastle(true);
+        return userId;
     }
 }
