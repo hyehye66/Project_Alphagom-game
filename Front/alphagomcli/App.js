b40c5ab6faac953616943f3e0af136c2d6a898cd
foursoob <foursoob@gmail.com>
2022-09-26 21:07:44 +0900

    [S07P22D210-399] App.js 통합

--- Front/alphagomcli/App.js
+++ Front/alphagomcli/App.js
@@ -1,3 +1,22 @@
-import App from './src/App';
+import { View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DrawerNavigator from './src/screen/otherpage/DrawerNavigator';
+
+
+function App () {
+
+    return (
+    <NavigationContainer>
+        <StatusBar hidden />
+        <DrawerNavigator />
+    </NavigationContainer>
+    );
+};
 
 export default App;
