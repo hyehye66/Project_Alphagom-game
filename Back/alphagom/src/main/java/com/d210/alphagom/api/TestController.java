1cad61ec81792b2330dc994b3cff2c01595e36e6
이미현 <mihyun0921@gmail.com>
2022-09-26 09:15:57 +0900

    setting - 자동빌드 설정 및 swagger 주석 달기

--- Back/alphagom/src/main/java/com/d210/alphagom/api/TestController.java
+++ Back/alphagom/src/main/java/com/d210/alphagom/api/TestController.java
@@ -1,5 +1,7 @@
 package com.d210.alphagom.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,12 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
+@Tag(name = "Test", description = "Test api 입니다.")
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestController {
+
+    @Operation(summary = "test", description = "test api 입니다.")
     @GetMapping("/")
     private ResponseEntity<?> test() {
         Map<String, Object> result = new HashMap<>();
--- Back/alphagom/src/main/java/com/d210/alphagom/api/UserApiController.java
+++ Back/alphagom/src/main/java/com/d210/alphagom/api/UserApiController.java
@@ -4,11 +4,14 @@ import com.d210.alphagom.api.dto.ResponseDTO;
 import com.d210.alphagom.api.dto.UserResponse;
 import com.d210.alphagom.domain.entity.User;
 import com.d210.alphagom.domain.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "User", description = "User api 입니다.")
 @Slf4j
 @RestController
 @RequestMapping("/api/be")
@@ -17,6 +20,7 @@ public class UserApiController {
 
     private final UserService userService;
 
+    @Operation(summary = "유저 정보 조회", description = "유저 정보 조회 api 입니다.")
     @GetMapping("/user/{userId}")
     public ResponseEntity<UserResponse> getUserInfo(@PathVariable Long userId) {
 
@@ -25,6 +29,7 @@ public class UserApiController {
         return ResponseEntity.ok(new UserResponse(user.getName()));
     }
 
+    @Operation(summary = "유저 닉네임 저장", description = "유저 닉네임 저장 api 입니다.")
     @PostMapping("/user/{userId}/nickname/{nickname}")
     public ResponseEntity<?> saveNickName(@PathVariable Long userId, @PathVariable String nickname) {
         
@@ -33,6 +38,7 @@ public class UserApiController {
         return ResponseEntity.ok(new ResponseDTO("닉네임이 저장되었습니다"));
     }
 
+    @Operation(summary = "튜토리얼 통과 여부 저장", description = "튜토리얼 통과 여부 저장 api 입니다.")
     @PutMapping("/user/{userId}/castle")
     public ResponseEntity<?> checkPassCastle(@PathVariable Long userId) {
         
