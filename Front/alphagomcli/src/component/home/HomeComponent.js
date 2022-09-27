dd6f06880cbbe7ce5e7c876e83c79ad0b09ff388
GyeongYeol <hrq789@naver.com>
2022-09-27 21:36:53 +0900

    [S07P22D210-424] feature - HomeComponent StyleSheet import

--- Front/alphagomcli/src/component/home/HomeComponent.js
+++ Front/alphagomcli/src/component/home/HomeComponent.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
