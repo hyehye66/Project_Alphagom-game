136bb106555bd9ccfdf580155721c1cb94394fa4
김수빈 <foursoob@naver.com>
2022-10-01 00:36:05 +0900

    Feature/front/map view

--- Front/alphagom/src/components/NavBar.vue
+++ Front/alphagom/src/components/NavBar.vue
@@ -1,68 +1,100 @@
 <template>
   <div class="navBar">
     <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
-      sidebar
-      <div class="logo">
+      <!-- sidebar -->
+      <!-- <div class="logo">
         <img src="@/assets/image/알파곰_표정_기본.png" alt="말해봐요 알파곰" /> 
-      </div>
+      </div> -->
 
       <div class="menu-toggle-wrap">
+        <div class="user-info">
+          <img class="user-img" src="@/assets/image/자라 표정 당당.png" alt="프로필 사진" height="20" />
+          <text class="user-name">{{username}} 님</text>
+        </div>
         <button
           class="menu-toggle"
-          type="button"
           style="background-color: transparent; border: 0; outline: 0;"
           @click="ToggleMenu">
-          <img src="@/assets/bars-3.svg" />
+          <Bars3Icon class="material-icons" style="height: 20px;"/>
         </button>
       </div>
 
-      <!-- <h3>Menu</h3> -->
-      <!-- <div class="menu"> -->
-        <!-- <router-link to="/" class="button">
-          <HomeIcon class="h-6 w-6 text-blue-500 material-icons" />
+      <!-- <h3>메뉴</h3> -->
+      <div class="menu">
+        <router-link to="/" class="button">
+          <HomeIcon class="material-icons" />
           <span class="text">처음으로</span>
         </router-link>
         <router-link to="/map" class="button">
-          <MapIcon class="h-6 w-6 text-blue-500 material-icons" />
+          <MapIcon class="material-icons" />
           <span class="text">탐험하기</span>
         </router-link>
         <router-link to="/myPage" class="button">
-          <UserIcon class="h-6 w-6 text-blue-500 material-icons" />
+          <UserIcon class="material-icons" />
           <span class="text">내 정보</span>
         </router-link>
         <router-link to="/setting" class="button">
-          <Cog6ToothIcon class="h-6 w-6 text-blue-500 material-icons" />
+          <Cog6ToothIcon class="material-icons" />
           <span class="text">설정하기</span>
-        </router-link> -->
+        </router-link>
+
+
+        <!-- 유저 정보 들어오면 수정하세용 -->
+
         <!-- <router-link to="/logout" class="button">
-          <span class="material-icons">email</span>
+          <ArrowLeftOnRectangleIcon class="material-icons" />
           <span class="text">로그아웃</span>
         </router-link> -->
-      <!-- </div> -->
+      </div>
     </aside>
   </div>
 </template>
 
 <script>
-import { HomeIcon, MapIcon, UserIcon, Cog6ToothIcon, Bars3Icon } from '@heroicons/vue/24/outline'
+import { HomeIcon, MapIcon, UserIcon, Cog6ToothIcon, Bars3Icon, ArrowLeftOnRectangleIcon } from '@heroicons/vue/24/outline'
+import { useAuthStore } from "@/stores/auth";
 
 export default {
-  components: { HomeIcon, MapIcon, UserIcon, Cog6ToothIcon, Bars3Icon }
+  components: { HomeIcon, MapIcon, UserIcon, Cog6ToothIcon, Bars3Icon, ArrowLeftOnRectangleIcon },
+  setup() {
+    const store = useAuthStore();
+    const username = store.username;
+    console.log(username)
+    return {username}
+  }
 }
 </script>
 
 <script setup>
 import { ref } from 'vue'
+import { useAuthStore } from "@/stores/auth";
 
 const is_expanded = ref(localStorage.getItem("is_expanded") === "true")
 
 const ToggleMenu = () => {
+  console.log('ToggleMenu func start')
 	is_expanded.value = !is_expanded.value
 	localStorage.setItem("is_expanded", is_expanded.value)
+  console.log(is_expanded.value)
+  console.log('ToggleMenu func end')
 }
+
+const store = useAuthStore();
+const username = store.username;
+console.log(username)
+
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+:root {
+  --light: #483737;
+  --dark: #FDF8E2;
+  --primary: #94C178;
+  --grey: #5E5E5E;
+  --dark-alt: #7D4D87;
+  --sidebar-width: 170px;
+
+}
 aside {
   position: absolute;
   z-index: 99;
@@ -74,8 +106,8 @@ aside {
   padding: 1rem;
   margin: 0px;
 
-  background-color: cadetblue;
-  color: beige;
+  background-color: transparent;
+  color: var(--light);
 
   transition: 0.2s ease-out;
 
@@ -90,20 +122,114 @@ aside {
   // 바 접힘
   .menu-toggle-wrap {
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
+    align-items: center;
     margin-bottom: 1rem;
 
     position: relative;
     top: 0;
     transition: 0.2s ease-out;
+
+    .user-info {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      
+      .user-img {
+        margin-top: 4.7px;
+        margin-right: 6px;
+      }
+      .user-name {
+        font-size: 0.75rem;
+        font-weight: 600;
+        letter-spacing: -1px;
+        margin-top: 5.5px;
+      }
+    }
+    .menu-toggle {
+      transition: 0.2s ease-out;
+
+      .material-icons {
+        font-size: 2rem;
+        color: var(--light);
+        transform: 0.2s ease-out;
+      }
+
+      &:hover {
+        .material-icons {
+          color: var(--primary);
+          // transform: translatex(0.5rem);
+        }
+      }
+    }
+  }
+  
+  h3, .button, .text, .user-name {
+    opacity: 0;
+    transition: 0.3s ease-out;
+  }
+  h3 {
+    color: var(--grey);
+    font-size: 0.875rem;
+    margin-bottom: 0.5rem;
+    text-transform: uppercase;
+  }
+  .menu {
+    margin: 0 -1rem;
+
+    .button {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+
+      padding: 0,5rem 1rem;
+      transition: 0.2s ease-out;
+
+      .material-icons {
+        font-size: 2rem;
+        height: 1.5rem;
+        color: var(--light);
+        transition: 0.2s ease-out;
+        margin: 0.25rem;
+        margin-left: 1rem;
+      }
+      .text {
+        color: var(--light);
+        transition: 0.2s ease-out;
+        font-size: 0.85rem;
+      }
+
+      &:hover, ~&.router-link-exact-active {
+        background-color: var(--dark-alt);
+        .material-icons, .text {
+          color: var(--primary);
+        }
+      }
+
+      &.router-link-exact-active {
+        border-right: 5px solid var(--primary);
+      }
+    }
   }
 
   // 바 열림
-  &.is_expanded {
-    widows: var(--sidebar-width);
+  &.is-expanded {
+    width: var(--sidebar-width);
+    background-color: var(--dark);
 
     .menu-toggle-wrap {
-      top: -3rem;
+      // top: -3rem;
+      .menu-toggle {
+        transform: rotate(-180deg);
+      }
+    }
+    h3, .button, .text, .user-name {
+      opacity: 1;
+    }
+    .button {
+      .material-icons {
+        margin-right: 1rem;
+      }
     }
   }
 
@@ -113,122 +239,4 @@ aside {
     
   }
 }
-// aside {
-// 	display: flex;
-// 	flex-direction: column;
-// 	background-color: green;
-// 	color: var(--light);
-// 	// width: calc(50px);
-//   width: 40px;
-// 	overflow: hidden;
-// 	min-height: 100vh;
-// 	padding: 1rem;
-// 	transition: 0.2s ease-in-out;
-//   align-items: center;
-// 	.flex {
-// 		flex: 1 1 0%;
-// 	}
-// 	.logo {
-// 		// margin-bottom: 1rem;
-// 		img {
-// 			width: 20px;
-// 		}
-// 	}
-// 	.menu-toggle-wrap {
-// 		display: flex;
-// 		justify-content: flex-end;
-// 		// margin-bottom: 1em;
-// 		position: relative;
-// 		top: 0;
-// 		transition: 0.4s ease-in-out;
-// 		.menu-toggle {
-// 			transition: 0.4s ease-in-out;
-// 			.material-icons {
-// 				font-size: 12px;
-// 				color: var(--light);
-// 				transition: 0.2s ease-out;
-// 			}
-			
-// 			&:hover {
-// 				.material-icons {
-// 					color: var(--primary);
-// 					transform: translateX(0.5rem);
-// 				}
-// 			}
-// 		}
-// 	}
-// 	h3, .button .text {
-// 		opacity: 0;
-// 		transition: opacity 0.3s ease-in-out;
-// 	}
-// 	h3 {
-// 		color: var(--grey);
-// 		font-size: 10px;
-// 		// margin-bottom: 0.5rem;
-// 		text-transform: uppercase;
-// 	}
-// 	.menu {
-// 		margin: 0 -1rem;
-//     align-items: centers;
-// 		.button {
-// 			display: flex;
-// 			align-items: center;
-// 			text-decoration: none;
-// 			transition: 0.2s ease-in-out;
-// 			padding: 0px 0px;
-//       background-color: pink;
-// 			.material-icons {
-// 				font-size: 12px;
-// 				color: var(--light);
-// 				transition: 0.2s ease-in-out;
-// 			}
-// 			.text {
-// 				color: var(--light);
-// 				transition: 0.2s ease-in-out;
-//         font-size: 12px;
-// 			}
-// 			&:hover {
-// 				background-color: var(--dark-alt);
-// 				.material-icons, .text {
-// 					color: var(--primary);
-// 				}
-// 			}
-// 			&.router-link-exact-active {
-// 				background-color: var(--dark-alt);
-// 				border-right: 5px solid var(--primary);
-// 				.material-icons, .text {
-// 					color: var(--primary);
-// 				}
-// 			}
-// 		}
-// 	}
-// 	&.is-expanded {
-// 		// width: var(--sidebar-width);
-//     width: 100px;
-// 		.menu-toggle-wrap {
-// 			// top: -3rems;
-			
-// 			.menu-toggle {
-// 				transform: rotate(-180deg);
-// 			}
-// 		}
-// 		h3, .button .text {
-// 			opacity: 1;
-// 		}
-// 		.button {
-//       background-color: palevioletred;
-// 			.material-icons {
-// 				margin-right: 10px;
-//         width: 20px;
-// 			}
-// 		}
-// 		.footer {
-// 			opacity: 0;
-// 		}
-// 	}
-// 	@media (max-width: 926px) {
-// 		position: absolute;
-// 		z-index: 99;
-// 	}
-// }
 </style>
