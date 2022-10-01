7f99e373f18ced5ada70c8a1212d3fb2f41bd0b4
foursoob <foursoob@gmail.com>
2022-10-01 14:37:06 +0900

    [] feature: store/mypage.ts 생성 및 테스트

--- Front/alphagom/src/views/MagicCastle/MagicCastleStartView.vue
+++ Front/alphagom/src/views/MagicCastle/MagicCastleStartView.vue
@@ -37,12 +37,12 @@ export default {
     game.setStage(stage.name);
 
     // 3초 후 다음 뷰로 넘기기
-    setTimeout(() => {
-      router.push({
-        name: "MagicCastleDialogView",
-        params: { dialogNum: 0 },
-      });
-    }, 3000);
+    // setTimeout(() => {
+    //   router.push({
+    //     name: "MagicCastleDialogView",
+    //     params: { dialogNum: 0 },
+    //   });
+    // }, 3000);
 
     return { stage };
   },
