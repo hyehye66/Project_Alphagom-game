87af3c829629948b3f04a3eda3e7a54c46ec4cab
foursoob <foursoob@gmail.com>
2022-10-02 04:41:14 +0900

    [] fix: isLoggedIn dummy data

--- Front/alphagom/src/stores/auth.ts
+++ Front/alphagom/src/stores/auth.ts
@@ -7,54 +7,55 @@ import { defineStore } from "pinia";
 // pinia는 index.js 없이 모듈화 된 파일만 있으면 된다.
 
 // 이름을 가지는 내보내기를 할 때 보통 이름 앞에 use를 붙여 사용한다.
-// export const useAuthStore = defineStore("auth", () => {
+export const useAuthStore = defineStore("auth", () => {
   // state
-  // const userInfo = ref(null); // 사용자 정보
-  // const token = ref(localStorage.getItem("token") || "");
+  const userInfo = ref(null); // 사용자 정보
+  const token = ref(localStorage.getItem("token") || "");
 
   // getters
-  // const recentUserInfo = computed(() => userInfo);
-  // const recentToken = computed(() => token);
+  const recentUserInfo = computed(() => userInfo);
+  const recentToken = computed(() => token);
   // const isLoggedIn = computed(() => !!token.value);
-  // const authHeader = computed(() => token.value ? { Authorization: `Bearer ${token.value}` } : "");
+  const isLoggedIn = true;
+  const authHeader = computed(() => token.value ? { Authorization: `Bearer ${token.value}` } : "");
 
   // actions
-  // function fetchUserInfo() {
+  function fetchUserInfo() {
     // console.log('userInfo.value: ' + userInfo.value)
     // console.log('recentUserInfo.value: ' + recentUserInfo.value)
     // console.log('recentUserInfo.effect: ' + recentUserInfo.effect.active)
 
-    // console.log('prototype: ' + authHeader.value.valueOf.prototype)
-    // console.log('toString: ' + authHeader.value.toString)
-    // console.log('authHeader: ' + authHeader.value)
+    console.log('prototype: ' + authHeader.value.valueOf.prototype)
+    console.log('toString: ' + authHeader.value.toString)
+    console.log('authHeader: ' + authHeader.value)
 
-    // axios({
-      // url: api.user.getUser(userInfo.value.userId),
+    axios({
+      url: api.user.getUser(userInfo.value.userId),
       // url: "http://localhost:8080/api/be/user/1234",
-      // method: 'get',
+      method: 'get',
       // headers: authHeader
-      // headers: authHeader.value.valueOf.prototype,
-    // })
-      // .then(res => {
-      //   userInfo.value = res.data.body.data;
-      // })
-      // .catch(err => {
-      //   console.error(err.response);
-      // });
-  // }
+      headers: authHeader.value.valueOf.prototype,
+    })
+      .then(res => {
+        userInfo.value = res.data.body.data;
+      })
+      .catch(err => {
+        console.error(err.response);
+      });
+  }
 
-//   return {
-//     // state
-//     userInfo,
-//     token,
+  return {
+    // state
+    userInfo,
+    token,
 
-//     // getters
-//     recentUserInfo,
-//     recentToken,
-//     isLoggedIn,
-//     authHeader,
+    // getters
+    recentUserInfo,
+    recentToken,
+    isLoggedIn,
+    authHeader,
 
-//     // actions
-//     fetchUserInfo,
-//   }
-// });
+    // actions
+    fetchUserInfo,
+  }
+});
