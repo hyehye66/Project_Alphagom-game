4a434bc70592c8c53e0212777a174d158fe5bc76
foursoob <foursoob@gmail.com>
2022-10-02 03:44:02 +0900

    [] feature: bg test

--- Front/alphagom/src/views/MapView.vue
+++ Front/alphagom/src/views/MapView.vue
@@ -45,42 +45,32 @@
     
   </div>
 </template>
-<script>
+<script setup>
 import { useRouter, useRoute } from 'vue-router';
 
-export default {
-  name: 'MapView',
-  components: {
-  },
-  setup() {
-    const router = useRouter();
-    const route = useRoute();
-    const clickMapItem = name => {
-      switch (name) {
-        case 'magicCastleStartView':
-        case 'swampStartView':
-        case 'darkCaveStartView':
-        case 'skyStartView':
-          router.push({ name });
-          break;
-        default:
-          break;
-      }
-    }
-    return {
-      clickMapItem
-    }
-    
+const router = useRouter();
+const route = useRoute();
+const clickMapItem = name => {
+  switch (name) {
+    case 'magicCastleStartView':
+    case 'swampStartView':
+    case 'darkCaveStartView':
+    case 'skyStartView':
+      router.push({ name });
+      break;
+    default:
+      break;
   }
-};
+}
+
 </script>
 
 <style scoped>
 .container-bg {
   position: absolute;
-  background-image: url("@/assets/image/지도_배경_사진_필터_low.png");
+  /* background-image: url("@/assets/image/지도_배경_사진_필터_low.png");
   background-repeat: no-repeat;
-  background-size: inherit;
+  background-size: inherit; */
   /* Add the blur effect */
   filter: blur(4px);
   top: 0px;
