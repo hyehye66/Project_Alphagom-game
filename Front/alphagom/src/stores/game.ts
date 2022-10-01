efd86353d0c084c6407134f742f4049ebbaf4334
mhlee <mihyun0921@gmail.com>
2022-10-02 00:10:10 +0900

    fix - fix npm build error

--- Front/alphagom/src/stores/game.ts
+++ Front/alphagom/src/stores/game.ts
@@ -8,6 +8,7 @@ import DarkcaveLine from "@/assets/dialog/DarkcaveLine.json";
 import SkyLine from "@/assets/dialog/SkyLine.json";
 import SwampLine from "@/assets/dialog/SwampLine.json";
 import router from "@/router";
+import { stringLiteral } from "@babel/types";
 
 // vuex 를 사용할 대는 store/index.js 파일이 필요했지만,
 // pinia는 index.js 없이 모듈화 된 파일만 있으면 된다.
@@ -17,7 +18,7 @@ import router from "@/router";
 export const useGameStore = defineStore("game", () => {
   /* state */
   const stage = ref(""); // 해당 스테이지 이름
-  const dialog = ref({}); //
+  const dialog = ref();
   const scriptNum = ref(0); // 현재 스크립트 번호
   const SwampScore = ref(5000); // 게임별 점수
   const DarkCaveScore = ref(2500);
@@ -48,7 +49,7 @@ export const useGameStore = defineStore("game", () => {
   // });
 
   // 현재 스크립트
-  const script = computed(() => dialog.value?.script[scriptNum.value]);
+  const script = computed(() => dialog.value.script[scriptNum.value]);
   // 현재 effect
   const effect = computed(() => script.value.effect);
   // 현재 type
@@ -102,97 +103,3 @@ export const useGameStore = defineStore("game", () => {
     setStage,
   };
 });
-
-// export const useGameStore = defineStore("game", {
-//   actions: {
-//     setStage(stage: string) {
-
-//       // start page에서 stage 이름 초기화
-//       this.stage = stage;
-//     },
-
-//     setType(type: string) {
-
-//       // script의 type로 초기화
-//       this.type = type;
-//     },
-//     setEffect(effect: string) {
-
-//       // script의 effect로 초기화
-//       this.effect = effect;
-//     },
-//     setGameList(stage: string) {
-
-//       // gameList set
-//       switch(stage) {
-//         case "sky":
-//           this.stageGames = this.dialogDict.sky;
-//           break;
-//         case "darkcave":
-//           this.stageGames = this.dialogDict.darkcave;
-//           break;
-//         case "swamp":
-//           this.stageGames = this.dialogDict.swamp;
-//           break;
-//       }
-//     },
-//     getNextScript() {
-
-//       console.log("클클클릭");
-
-//       // script type이 story일 때 다음 스크립트 호출
-//       if (this.type === "story") {
-
-//         // scriptNum += 1
-//         this.scriptNum++;
-
-//         // 다음 스크립트 호출 & type 및 effect 초기화
-//         const nextScript = this.dialog.script[this.scriptNum];
-//         this.setType(nextScript.type);
-//         this.setEffect(nextScript.effect);
-//       }
-
-//       // 게임 route
-//       if (this.type === "game") {
-
-//         // scriptNum += 1
-//         this.scriptNum++;
-
-//         // 다음 스크립트 호출 & type 및 effect 초기화
-//         const nextScript = this.dialog.script[this.scriptNum];
-//         this.setType(nextScript.type);
-//         this.setEffect(nextScript.effect);
-
-//         const gameType = this.stageGames[0];
-//         this.stageGames.pop();
-//         router.push(gameType);
-//       }
-
-//       if (this.type === "question") {
-
-//       }
-
-//       if (this.type === "end") {
-
-//       }
-//     },
-//     beforeScript() {
-//       // 이전 스크립트로
-//       this.scriptNum--;
-//     },
-//     nextStage() {},
-//     getDialog() {
-//       // 해당 스테이지의 전체 대화를 가져오는 함수
-//       this.dialogList.forEach((element) => {
-//         if (element.stage == this.stage) {
-//           this.dialog = element;
-//         }
-//       });
-//     },
-//       //   router.push("stageChangeView");
-//       //   return;
-//       // }
-//       // return this.dialog[this.stageStatus][this.scriptNum];
-//     // },
-//   },
-// });
