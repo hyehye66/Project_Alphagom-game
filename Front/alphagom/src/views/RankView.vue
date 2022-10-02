18f3c6bab2991c753e997e2f493b410826823041
김수빈 <foursoob@naver.com>
2022-10-03 00:00:40 +0900

    [] feature: views 바깥 파일들 배경 깔았음

--- Front/alphagom/src/views/RankView.vue
+++ Front/alphagom/src/views/RankView.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
-    <NavBar></NavBar>
+    <div class="container-bg"></div>
   </div>
 </template>
 
-<script>
-import NavBar from '@/components/NavBar.vue';
+<script setup>
+import { useBgStore } from "@/stores/bg"
+import { computed, onMounted } from 'vue'
 
-export default {
-  name: 'RankView',
-  components: {
-    NavBar,
-  },
-  setup() {
-      
-  }
-
-}
+// 배경 경로 수정
+onMounted (() => {
+  // store의 bgUrlState 값을 직접 변경
+  bgStore.bgUrlState = 'url("/map_bg_picture_filter_low.png")'
+  console.log(bgStore.bgUrlState.value)
+})
+const bgStore = useBgStore()
+// state 감시자
+const bgwatching = computed(() => bgStore.bgUrlState)
 </script>
 
 <style scoped>
-  
+.container-bg {
+  position: absolute;
+  background-color: transparent;
+  backdrop-filter: blur(4px);
+  top: 0px;
+  left: 0px;
+  width: 926px;
+  height: 428px;
+} 
 </style>
--- Front/alphagom/src/views/SettingView.vue
+++ Front/alphagom/src/views/SettingView.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
+    <div class="container-bg"></div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'SettingView',
-  components: {
-  },
-  setup() {
-    return {
-
-    }
-  }
+<script setup>
+  import { useBgStore } from "@/stores/bg"
+  import { computed, onMounted } from 'vue'
   
-}
-</script>
-
-
-<style scoped>
+  // 배경 경로 수정
+  onMounted (() => {
+    // store의 bgUrlState 값을 직접 변경
+    bgStore.bgUrlState = 'url("/map_bg_picture_filter_low.png")'
+    console.log(bgStore.bgUrlState.value)
+  })
+  const bgStore = useBgStore()
+  // state 감시자
+  const bgwatching = computed(() => bgStore.bgUrlState)
+  </script>
   
-</style>
+  <style scoped>
+  .container-bg {
+    position: absolute;
+    background-color: transparent;
+    backdrop-filter: blur(4px);
+    top: 0px;
+    left: 0px;
+    width: 926px;
+    height: 428px;
+  } 
+  </style>
