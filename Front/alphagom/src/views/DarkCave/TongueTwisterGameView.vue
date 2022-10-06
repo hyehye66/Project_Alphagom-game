418d4534153d33eee8a3fd2895c257269ddc06fc
배근혜 <odille2992@naver.com>
2022-10-07 08:29:16 +0900

    feature - 수정

--- Front/alphagom/src/views/DarkCave/TongueTwisterGameView.vue
+++ Front/alphagom/src/views/DarkCave/TongueTwisterGameView.vue
@@ -325,6 +325,11 @@ const getNextPage = () => {
   width: 926px;
   text-align: center;
 
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+  "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+  "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+  sans-serif;
+
 
 }
 .game-ex {
