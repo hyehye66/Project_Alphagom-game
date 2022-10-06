b37ef9ef8c09b8da99b9f4411431e4f20281e53e
배근혜 <odille2992@naver.com>
2022-10-06 14:34:48 +0900

    Feature/front/game score modal css

--- Front/alphagom/src/views/Modal/GameScoreModal.vue
+++ Front/alphagom/src/views/Modal/GameScoreModal.vue
@@ -1,22 +1,48 @@
 <template>
-  <div class="score-modal" tabindex="-1">
-    <div class="modal-content">
+  <div class="modal" tabindex="-1">
+    <div class="modal__modal-content">
+      <!-- 모달 박스 -->
+      <svg
+        class="modal__modalbox"
+        width="553"
+        height="251"
+        viewBox="0 0 553 251"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="2.5"
+          y="2.5"
+          width="548"
+          height="246"
+          rx="27.5"
+          fill="#94C178"
+          stroke="#FDF8E2"
+          stroke-width="5"
+          stroke-linecap="round"
+          stroke-dasharray="10 10"
+        />
+      </svg>
       <br>
-      <div class="modal-body">
-        <div>축하합니다!</div>
-        <!-- 점수 받아와야 됨 -->
-        <div>{{ score }} 점입니다!</div>
+      <!-- 모달 내용 -->
+      <div class="modal__modal-body">
+        <div class="modal__howto-title">축하합니다!</div>
+        <div class="modal__howto-content">
+          <div style="height: 6px"></div>
+          <div class="modal__howto-content-bold">{{ score }} 점</div>
+        </div>
       </div>
-      <div class="modal-footer">
+      <div class="modal__modal-footer">
         <button
           @click="getNextStage()"
-          class="next-btn"
+          class="modal__next-btn"
           data-bs-dismiss="modal"
         >
           다음 탐험으로
         </button>
       </div>
     </div>
+    <div class="modal__overlay" @click.self="$emit('close')"></div>
   </div>
 </template>
 
@@ -58,29 +84,104 @@ const getNextStage = () => {
 };
 </script>
 
-<style scoped>
-.score-modal {
-  height: 50vh;
+<style lang="scss" scoped>
+.modal {
+  position: absolute;
+  width: 553px;
+  height: 251px;
+  top: 95px;
+  left: 186px;
+  &__overlay {
+    position: absolute;
+    top: -100px;
+    left: -190px;
+    width: 100vw;
+    height: 100vh;
+
+    background-color: black;
+    opacity: 0.5;
+    // z-index: 1;
+  }
+  &__modal-content {
+    z-index: 1;
+    // z-index: 2;
+  }
+  &__modal-body {
+    display: flex;
+    flex-flow: column;
+    position: absolute;
+  }
+  &__howto-title {
+    font-size: 32px;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+    "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+    sans-serif;
+    font-weight: 800;
+    white-space: nowrap;
+    color: #FDF8E2;
+    // text-shadow: -2px 0 #345A49, 0 2px #345A49, 2px 0 #345A49, 0 -2px #345A49;
+    -webkit-text-stroke: 1.5px #34435A;
+
+    position: absolute;
+    top: -235px;
+    left: 158px;
+  }
+  &__howto-content {
+    font-size: 20px;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+    "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+    sans-serif;
+    font-weight: 500;
+    white-space: nowrap;
+    color: #34435A;
+    text-align: center;
+    line-height: 110%;
+
+    position: absolute;
+    top: -175px;
+    left: 135px;
+  }
+  &__howto-content-bold {
+    font-size: 60px;
+    font-weight: 800;
+  }
+  &__next-btn {
+    position: absolute;
+    top: -87px;
+    left: 202px;
+    width: 149.17px;
+    height: 48px;
+    /* margin-left: 19px; */
+
+    outline: none;
+    border: 5px solid #94c178;
+    border-radius: 30px;
+    background-color: #FAF4BD;
+    cursor: pointer;
+    box-shadow: 1px 1px 3px 1px gray;
+
+    font-size: 20px;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR",
+    "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+    sans-serif;
+    font-weight: 700;
+    white-space: nowrap;
+    color: #594640;
+  }
+
+
+
+  /* height: 50vh;
   border-radius: 18px;
   border: 18px dashed #faf4bd;
-  background-color: #94c178;
+  background-color: #94c178; */
 
-  display: flex;
+  /* display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: center; */
 }
 
-.modal-content {
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.modal-body {
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
-}
 </style>
